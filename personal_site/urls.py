8a4017d3d34d2c3fd4ff2b53a4353cf38374a2e5
k4u5h4L <kaushal.v.bhat@gmail.com>
2020-12-18 09:33:50 +0530

    Initial commit

---
+++ personal_site/urls.py
@@ -0,0 +1,39 @@
+"""personal_site URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, handler404, handler500
+
+from django.views.static import serve 
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('landing/', include('users.urls')),
+    path('blog/', include('blog.urls')),
+    path('', include('portfolio.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+# handler404 = 'forum.views.not_found_page'
+# handler500 = 'forum.views.not_found_page_server'
